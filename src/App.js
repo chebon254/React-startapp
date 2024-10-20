cb29f03bc2736056689658de867f19754266fd2e
chebon254 <kelvinchebon90@gmail.com>
2024-10-21 01:10:58 +0300

    Context

--- src/App.js
+++ src/App.js
@@ -2,28 +2,37 @@
 // import Main from './components/Main';
 // import Header from './components/Header';
 import './App.css';
+import MealList from './components/MealsList';
+import Counter from './components/Counter';
+import MealsProvider from './providers/MealsProvider';
 // import Btn from './components/Btn';
-import Heading from './Heading';
-import React, {useState} from 'react';
+// import Heading from './Heading';
+// import React, {useState} from 'react';
 // function Heading(props) {
 //   return <h1>Hello World {props.title}</h1>
 // }
 function App() {
-  const [word, setWord] = React.useState('Eat')
+  // const [word, setWord] = React.useState('Eat')
 
-  function handleClick() {
-    setWord('Drink')
-  }
+  // function handleClick() {
+  //   setWord('Drink')
+  // }
   return (
     <div>
-      <Heading message={word + " at kempinski"}/>
-      <button onClick={handleClick}>Click Here</button>
+      {/* <Heading message={word + " at kempinski"}/>
+      <button onClick={handleClick}>Click Here</button> */}
       {/* <Btn/> */}
       {/* <Heading/>
       <h1>Hello World {props.title}</h1> */}
       {/* <Header name="Chebon" color="Green"/>
       <Main />
       <Sidebar /> */}
+
+      <MealsProvider>
+        <MealList/>
+        <Counter/>
+      </MealsProvider>
+
     </div>
   );
 }
