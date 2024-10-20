d99dd32026ab27467f74b7b5e02673c3a7506866
chebon254 <kelvinchebon90@gmail.com>
2024-10-21 00:10:21 +0300

    Hooks

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@
 // import Main from './components/Main';
 // import Header from './components/Header';
 import './App.css';
-import Btn from './components/Btn';
-
+// import Btn from './components/Btn';
+import Heading from './Heading';
+import React, {useState} from 'react';
 // function Heading(props) {
 //   return <h1>Hello World {props.title}</h1>
 // }
 function App() {
+  const [word, setWord] = React.useState('Eat')
+
+  function handleClick() {
+    setWord('Drink')
+  }
   return (
     <div>
-      <Btn/>
+      <Heading message={word + " at kempinski"}/>
+      <button onClick={handleClick}>Click Here</button>
+      {/* <Btn/> */}
       {/* <Heading/>
       <h1>Hello World {props.title}</h1> */}
       {/* <Header name="Chebon" color="Green"/>
