b6bb1de0a7bd205cbe081c04819a1328b0412b3a
chebon254 <kelvinchebon90@gmail.com>
2024-10-22 02:59:29 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 // import Main from './components/Main';
 // import Header from './components/Header';
 import './App.css';
-import Asset from './assets/Asset.jpg'
-import ReactPlayer from "react-player"
+// import Asset from './assets/Asset.jpg'
+// import ReactPlayer from "react-player"
 // import Homepg from './Homepg';
 // import About from './About';
 // import { Routes, Route, Link } from 'react-router-dom';
@@ -21,9 +21,39 @@ function App() {
 
   // function handleClick() {
   //   setWord('Drink')
-  // }
-  const videoUrl = "https://youtu.be/cd3P3yXyx30"
+  // // }
+  // const videoUrl = "https://youtu.be/cd3P3yXyx30"
+  const data = [
+    {
+      id: "1",
+      title: "Title",
+      description: "Lorem Ipsum text",
+      price: "$50"
+    },
+    {
+      id: "2",
+      title: "Title2",
+      description: "Lorem Ipsum text2",
+      price: "$502"
+    },
+    {
+      id: "3",
+      title: "Title3",
+      description: "Lorem Ipsum text3",
+      price: "$530"
+    }
+  ]
+  const topDessert = data.map(dessert => {
+      return {
+      content: `${dessert.title} - ${dessert.description}`,
+      price: dessert.price,
+      }
+    }
+  )
+
+  console.log(topDessert);
   return (
+    
     <div>
       {/* <Heading message={word + " at kempinski"}/>
       <button onClick={handleClick}>Click Here</button> */}
@@ -48,7 +78,7 @@ function App() {
         <Route path='/about' errorElement={<About />} />
       </Routes> */}
       {/* <img src={Asset} alt="Monkey Image" /> */}
-      <ReactPlayer url={videoUrl} playing={false} volume={0.5}/>
+      {/* <ReactPlayer url={videoUrl} playing={false} volume={0.5}/> */}
     </div>
   );
 }
